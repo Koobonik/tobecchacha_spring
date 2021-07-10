313aa4be955cbcac8a742a3e80dd2174742bd27b
Koobonik <kbi960130@naver.com>
2021-07-10 22:47:39 +0900

    테스트 코드 삭제

--- src/test/java/com/spring/ApplicationTests.java
+++ src/test/java/com/spring/ApplicationTests.java
@@ -1,43 +1,7 @@
 package com.spring;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.spring.dto.requestDto.EmailSenderRequestDto;
-import com.spring.service.EmailService;
-import com.spring.service.SmsService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URISyntaxException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.text.ParseException;
-
-@SpringBootTest
 class ApplicationTests {
 
-
-    @Autowired
-    private EmailService emailService;
-
-    @Test
-    public void sendEmail() throws Exception {
-        // 변수는 차례대로 받는사람의 메일주소, 제목, 내용 입니다.
-        emailService.sendEmail(new EmailSenderRequestDto("kbi960130@naver.com", "안녕하십니까", "잘 보내집니다."));
-    }
-
-    @Autowired
-    private SmsService smsService;
-
-    @Test
-    void contextLoads() {
-        System.out.println("asdasd");
-    }
-
-    @Test
-    void sendSms() throws JsonProcessingException, ParseException, UnsupportedEncodingException, URISyntaxException, NoSuchAlgorithmException, InvalidKeyException {
-        smsService.sendSms("01011112222","오예");
-    }
 
 }
